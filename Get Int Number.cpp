bbef74351723c5f266f044c83c8c6cdff41f8ec2
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2018-11-17 17:53:06 -0800

    Create Get Int Number.cpp

---
+++ Get Int Number.cpp
@@ -0,0 +1,28 @@
+int getIntNumber()
+{
+	float value;
+	
+	do 
+	{
+		bool validInput;
+		
+		cin >> value;
+		validInput = cin.good();
+		
+		if (validInput)
+		{
+			if(value -((int) value) > 0)
+				cout << "Invalid Entry! Number needs to be an integer.";
+			else break;
+		}
+		else
+		{
+			cout << "The valid entries are integer numbers. "
+				 << "Please enter an integer.\n";
+			cin.clear();
+			cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
+		}
+	} while (true);
+	
+	return (int)value;
+}
